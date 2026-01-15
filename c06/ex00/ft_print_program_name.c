ce7d388a37ee1c9934e07770e605b734b6d6c2dd
saad zarouali <zaroualisaid57@gmail.com>
2026-01-15 20:55:50 +0100

    100%

--- c06/ex00/ft_print_program_name.c
+++ c06/ex00/ft_print_program_name.c
@@ -6,22 +6,28 @@
 /*   By: saad <saad@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 16:03:25 by saad              #+#    #+#             */
-/*   Updated: 2026/01/15 16:06:57 by saad             ###   ########.fr       */
+/*   Updated: 2026/01/15 18:44:32 by saad             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include<unistd.h>
+
+#include <unistd.h>
+
 void	ft_print_programme_name(char *str)
 {
-	int i = 0;
-	while(str[i])
+	int	i;
+
+	i = 0;
+	while (str[i])
 	{
 		write(1, &str[i], 1);
 		i++;
 	}
 }
-int	main(int argc , char **argv)
+
+int	main(int argc, char **argv)
 {
-	if(argc == 1)
-	ft_print_programme_name(argv[0]);
+	if (argc == 1)
+		ft_print_programme_name(argv[0]);
 	write(1, "\n", 1);
+	return (0);
 }
